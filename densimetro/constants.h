df9a28678920a4f1ff8712979148d5dac4a2088e
Pablo <pablodo@gmail.com>
2019-01-24 18:20:40 -0300

    Constantes para d2

--- densimetro/constants.h
+++ densimetro/constants.h
@@ -1,7 +1,7 @@
 #define TEMP_OFFSET -1600
 #define Z_ACCEL_OFFSET 1000
-#define GX_CAL 89
-#define GY_CAL 114
-#define GZ_CAL 33
-#define ERROR_PITCH_ACC 1
-#define ERROR_ROLL_ACC 87.01
+#define GX_CAL -222.22
+#define GY_CAL 50.22
+#define GZ_CAL -94.41
+#define ERROR_PITCH_ACC -0.4
+#define ERROR_ROLL_ACC 77.59
